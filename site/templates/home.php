2ce40c42297c15a52853057351f5b772a77ae455
Pablo H. Paladino <paladinop@gmail.com>
2014-08-14 12:21:15 -0400

    trama y mejora en animations

--- site/templates/home.php
+++ site/templates/home.php
@@ -12,53 +12,69 @@
                 data-center-top="background-position:0px 0px;" 
                 data-top-bottom="background-position:0px -50px;"
                 >
-                <div id="header-container" class="container">
-                    <h1><?php echo $page->title;?></h1>
-                </div>
-                <div class="destacado">
-                    <div class="container">
-                        <?php echo $page->body;?>
+                <div class="trama">
+
+                    <div id="header-container" class="container">
+                        <div class="row">
+                            <div class="col-md-8 col-md-offset-2">
+                                <h1><?php echo $page->title;?></h1>
+                            </div>    
+                        </div>
                     </div>
-                </div>
-                <!-- Static navbar -->
-                <div class="navbar-placeholder">
-                    <div class="navbar navbar-default" role="navigation">
+                    <div class="destacado">
                         <div class="container">
-                            <div class="navbar-header">
-                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                                    <span class="sr-only">Toggle navigation</span>
-                                    <span class="icon-bar"></span>
-                                    <span class="icon-bar"></span>
-                                    <span class="icon-bar"></span>
-                                </button>
+                            <div class="row">
+                                <div class="col-md-8 col-md-offset-2">
+                                    <?php echo $page->body;?>
+                                </div>
                             </div>
-                            <div class="navbar-collapse collapse">
-                                <ul class="nav navbar-nav">
-                                    <?php
-                                    $menu = '';
-                                    //Principal
-                                    foreach($root->children()->sort("order") as $principal) {
-                                        //Secundaria
-                                        $secundarias = $principal->children()->sort("order");
-                                        if($secundarias->count()){
-                                            $menu .= "<li class='dropdown'>
-                                                        <a id='section-{$principal->id}-anchor' href='javascript:;' class='dropdown-toggle' data-toggle='dropdown'>$principal->title<span class='caret'></span></a>
-                                                        <ul class='dropdown-menu' role='menu'>";
-                                        }else{
-                                            $menu .= "<li><a class='scrollToLink' href='#section-{$principal->id}'>{$principal->title}</a></li>";
-                                        }
-                                        foreach($secundarias as $secundaria){
-                                            $menu .= "<li><a class='scrollToLink scrollToLinkSecundaria' href='#section-{$secundaria->id}'>{$secundaria->title}</a></li>";
-                                        }
-                                        $menu .= ($secundarias->count())?'</ul></li>':'';
-                                    }
-                                    echo $menu;
-                                    ?>
-                                </ul>
-                            </div><!--/.nav-collapse -->
-                        </div><!--/.container-fluid -->
+                        </div>
                     </div>
-                </div>
+                    <!-- Static navbar -->
+                    <div class="navbar-placeholder">
+                        <div class="navbar navbar-default" role="navigation">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-8 col-md-offset-2">
+                                        <div class="navbar-header">
+                                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                                <span class="sr-only">Toggle navigation</span>
+                                                <span class="icon-bar"></span>
+                                                <span class="icon-bar"></span>
+                                                <span class="icon-bar"></span>
+                                            </button>
+                                        </div>
+                                        <div class="navbar-collapse collapse">
+                                            <ul class="nav navbar-nav">
+                                                <?php
+                                                $menu = '';
+                                                //Principal
+                                                foreach($root->children()->sort("order") as $principal) {
+                                                    //Secundaria
+                                                    $secundarias = $principal->children()->sort("order");
+                                                    if($secundarias->count()){
+                                                        $menu .= "<li class='dropdown'>
+                                                                    <a id='section-{$principal->id}-anchor' href='javascript:;' class='dropdown-toggle' data-toggle='dropdown'>$principal->title<span class='caret'></span></a>
+                                                                    <ul class='dropdown-menu' role='menu'>";
+                                                    }else{
+                                                        $menu .= "<li><a class='scrollToLink' href='#section-{$principal->id}'>{$principal->title}</a></li>";
+                                                    }
+                                                    foreach($secundarias as $secundaria){
+                                                        $menu .= "<li><a class='scrollToLink scrollToLinkSecundaria' href='#section-{$secundaria->id}'>{$secundaria->title}</a></li>";
+                                                    }
+                                                    $menu .= ($secundarias->count())?'</ul></li>':'';
+                                                }
+                                                echo $menu;
+                                                ?>
+                                            </ul>
+                                        </div><!--/.nav-collapse -->
+                                    </div>
+                                </div>
+                            </div><!--/.container-fluid -->
+                        </div>
+                    </div>
+
+                </div> <!--/.trama -->
             </div>
 
 
@@ -70,11 +86,12 @@
                     'data-center-top="background-position:0px 0px;"'.
                     'data-top-bottom="background-position:0px -500px;"'.
                 '>';
-                echo '<div class="container">';
+                echo '<div class="trama">';
+                echo '<div class="container"><div class="row"><div class="col-md-8 col-md-offset-2">';
                 echo    '<h1>'.$principal->title.'</h1>';
-                echo '</div>';
+                echo '</div></div></div>';
                 echo '<div class="destacado">';
-                echo    '<div class="container">';
+                echo    '<div class="container"><div class="row"><div class="col-md-8 col-md-offset-2">';
                 echo        $principal->body;
                     //Planes
                     $planes = $principal->list;
@@ -91,9 +108,9 @@
                         }
                         echo '</div>';
                     }
-                echo    '</div>';
-                echo '</div>';
-                echo '<div class="container">';
+                echo    '</div></div></div>'; // container
+                echo '</div>'; //destacado
+                echo '<div class="container"><div class="row"><div class="col-md-8 col-md-offset-2">';
 
                 //Secundaria
                 $secundarias = $principal->children()->sort("order");
@@ -108,8 +125,9 @@
                     echo '</div></div></div>';
                 }
 
-                echo '</div>';
-                echo '</div>';
+                echo    '</div></div></div>'; //container
+                echo '</div>'; //trama
+                echo '</div>';//section
             }
             ?>
 
